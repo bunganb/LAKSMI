b56d8d12f55eb995bba9c308be8724d216815087
bunganb <bunganabila1005@gmail.com>
2023-11-12 07:37:33 +0700

    asset: add font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./public/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        kufam: 'kufam',
+        dmSans: 'dm-sans',
+      },
+    },
   },
   plugins: [],
 };
