5e060ae5097d618aeccc0bc8f6bc4826e6669a59
bunganb <bunganabila1005@gmail.com>
2023-11-12 07:44:21 +0700

    asset: update color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ['./public/**/*.{html,js}'],
   theme: {
     extend: {
+      colors: {
+        primary: '#27AEA1',
+        secondary: '#E5BC40',
+        danger: '#D83F31',
+        dark: '#1E1E1E',
+        light: '#F9F9F9',
+      },
       fontFamily: {
         kufam: 'kufam',
         dmSans: 'dm-sans',
